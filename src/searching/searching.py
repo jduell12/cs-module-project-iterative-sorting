777cf9b81a49703aad1a1364c1a39374bc605717
Jessica <duellje@oregonstate.edu>
2020-09-14 13:03:44 -0400

    binary search passes tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,18 +3,42 @@ def linear_search(arr, target):
 
 
     return -1   # not found
+"""
+Linear Search - O(log n)
+
+1. Start from index 0 
+2. Check if the element at the index is equal to the target value 
+3. If the element at the index is equal return the index 
+4. Otherwise go to next index. If next index is out of bounds return -1 
+
+"""
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    #keep track of left and right edges
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        #find midpoint element 
+        # length // 2 (integer division - no floats after division)
+        midpoint = ((right + left) //2)
+        #check if midpoint
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] < target:
+            #toss out the left side of the array by changing left variable
+            # add 1 to midpoint since we know the midpoint is not the target so we don't need to include it
+            left = midpoint + 1
+        else:
+            #toss out the right side of the array without including the midpoint element
+            right = midpoint - 1 
+            
     return -1  # not found
 
 """
-Binary Search
+Binary Search - O(log n)
 
 1. Find the midpoint element 
     - round down if even elements 
@@ -23,7 +47,12 @@ Binary Search
 4. Otherwise, determine which direction the binary search needs to go in
 5. Repeat from Step 1 until find the target or if target not in array return -1 
 
+Binary Search O(log n)
+Inserting into BST O(log n)
 
+Total Runtime complexity: O(log n * log n) = O (log^2 n)
+
+Ex. 
 [3, 4, 6, 16, 26, 28, 52, 55]
   0 1  2  3   4   5   6   7
 target: 55
